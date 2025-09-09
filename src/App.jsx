7fda4c310e7ce2d04916787ad526064301bd817a
aminur-git <aminur.4mi@gmail.com>
2025-09-10 01:47:08 +0600

    search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 
+import { useState } from 'react'
 import './App.css'
+import Search from './Components/Search'
 
 const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState('')
+
   return (
     <main>
       <div className='pattern'>
@@ -10,7 +15,8 @@ const App = () => {
       <div className='wrapper'>
         <img src="./hero.png" alt="" />
         <h1>Find <span className='text-gradient'>Movies</span> You Will Enjoy Without Hassle.</h1>
-        <p>Search</p>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className='text-white'>{searchTerm}</h1>
       </div>
     </main>
   )
