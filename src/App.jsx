6445fc745a59463f9809c0f402bb91d71cc65fc9
aminur-git <aminur.4mi@gmail.com>
2025-09-10 01:32:58 +0600

    header and some assets

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ const App = () => {
 
       </div>
       <div className='wrapper'>
+        <img src="./hero.png" alt="" />
         <h1>Find <span className='text-gradient'>Movies</span> You Will Enjoy Without Hassle.</h1>
+        <p>Search</p>
       </div>
     </main>
   )
